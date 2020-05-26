4510fa1971aee4a1499b56e7cdc340af0d490b58
Matthew McFarlane <masaki@matthews-mbp.lan>
2020-05-26 13:27:09 -0700

    Added Home page image

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -5,7 +5,10 @@ export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4">Food Recipes</h1>
+      <img
+        src={"https://wallup.net/wp-content/uploads/2017/05/29/206588-samurai-Japan-dragon-748x421.png"}
+      />
+        <h1 className="display-4">Matthew's Recipes</h1>
         <p className="lead">
           A curated list of recipes for the best homemade meal and delicacies.
         </p>
